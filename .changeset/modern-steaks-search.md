80995661d344436c794d633fad100621781e14c7
Morten Jensen <mje@monthio.com>
2023-07-21 09:43:30 +0200

    Changes to usePagination instead of Paginator from core

---
+++ .changeset/modern-steaks-search.md
@@ -0,0 +1,6 @@
+---
+"@jensen95/advanced-checkbox": minor
+"@jensen95/advanced-select": minor
+---
+
+Changes to usePagination instead of Paginator from core
--- packages/advanced-select/src/advancedSelect.ts
+++ packages/advanced-select/src/advancedSelect.ts
@@ -4,12 +4,12 @@ import {
   useRef,
   useKeypress,
   usePrefix,
+  usePagination,
   isUpKey,
   isDownKey,
   isSpaceKey,
   isBackspaceKey,
   isEnterKey,
-  Paginator,
   Separator,
 } from "@inquirer/core";
 import chalk from "chalk";
@@ -46,7 +46,6 @@ export const advancedCheckboxPrompt = createPrompt(
     done: (value: Array<Value>) => void
   ): string => {
     const { prefix = usePrefix(), instructions, pageSize } = config;
-    const paginator = useRef(new Paginator()).current;
     const initialChoices = useRef(
       config.choices.map(
         (choice, choiceIndex) =>
@@ -178,11 +177,10 @@ export const advancedCheckboxPrompt = createPrompt(
       })
       .join("\n");
 
-    const windowedChoices = paginator.paginate(
-      allChoices,
-      cursorPosition,
-      pageSize
-    );
+    const windowedChoices = usePagination(allChoices, {
+      active: cursorPosition,
+      pageSize,
+    });
     return `${prefix} ${message}${
       search.length > 0 ? " " + chalk.yellow(search) : ""
     }${helpTip}\n${windowedChoices}${ansiEscapes.cursorHide}`;
