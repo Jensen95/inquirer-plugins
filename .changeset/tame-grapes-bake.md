efdbf377e46dcd174130b3bf6e2884c2cc9c39bb
Morten Jensen <mje@monthio.com>
2024-01-29 13:42:52 +0100

    Adding is vim arrow binding, makes it possible to share logic for checking if it's a vim binding for up and down key

---
+++ .changeset/tame-grapes-bake.md
@@ -0,0 +1,5 @@
+---
+"is-vim-arrow-binding": patch
+---
+
+Adding is vim arrow binding, makes it possible to share logic for checking if it's a vim binding for up and down key
---
+++ packages/is-vim-arrow-binding/src/isVimArrowBinding.ts
@@ -0,0 +1,18 @@
+interface KeypressEvent {
+  name: string;
+  ctrl: boolean;
+}
+
+export const isVimArrowBinding = (key: KeypressEvent): boolean => {
+  if (key.ctrl) {
+    return false;
+  }
+
+  switch (key.name) {
+    case "j":
+    case "k":
+      return true;
+    default:
+      return false;
+  }
+};
